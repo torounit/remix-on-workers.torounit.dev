e4244addd0deef571790cb486744e665d6d0be11
Hiroshi Urabe <mail@torounit.com>
2023-10-04 22:00:32 +0900

    remove api-fetch

--- app/apiFetch.tsx
+++
@@ -1,7 +0,0 @@
-import apiFetch from '@wordpress/api-fetch';
-
-export const setRoot = ( url: string ) => {
-  apiFetch.use( apiFetch.createRootURLMiddleware( `${url}/wp-json/` ) );
-}
-
-export default apiFetch;
